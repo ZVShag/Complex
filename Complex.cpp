e04917c2e79ff0c17e2348a3af30dad4928de1e2
MarshallMathers1313 <AshurovAbdulla13>
2022-03-09 18:17:08 +0500

    Добавьте файлы проекта.

---
+++ Complex.cpp
@@ -0,0 +1,18 @@
+﻿// Complex.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+class Complex
+{
+    float vesh;
+    int mnim;
+public:
+    Complex(float a, int b) { vesh = a; mnim = b; }
+    
+
+};
+int main()
+{
+    
+}
+
