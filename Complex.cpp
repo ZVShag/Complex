fc0598f4d577d0c284c4cfab0ab1129131c5c665
MarshallMathers1313 <AshurovAbdulla13>
2022-03-09 18:26:25 +0500

    Добавлен метод нахождения суммы

--- Complex.cpp
+++ Complex.cpp
@@ -1,11 +1,17 @@
 ﻿
 #include <iostream>
+using namespace std;
 class Complex
 {
     float vesh;
     int mnim;
 public:
     Complex(float a, int b) { vesh = a; mnim = b; }
+
+    void Compsum(Complex a, Complex b)
+    {
+        cout << a.vesh + b.vesh << a.mnim + b.mnim;
+    }
 
 };
 int main()
